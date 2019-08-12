e1135ebf5ab3360f39a7ffc1832675df6546d293
shaquima <senoritabroccolini@gmail.com>
2019-08-11 23:49:08 -0500

    Updated README

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # FriendFinder
+Full stack, compatability based friend-matching application.
+
+# How to Use
+1. Clone repo to your machine
+2. Run npm install
+3. Serve localhost:8080
+
+# Tech Used:
+Javascript, node.js, HTML, CSS, Express.js, 
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var path = require("path");
 var bodyParser = require("body-parser");
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
